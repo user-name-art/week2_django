bc333dc6ff5f31c40841bff350c37095ada75f9c
Art Gilyazov <art.gilyazov@mail.ru>
2023-08-22 07:55:09 +0300

    add get_object_or_404

--- datacenter/passcard_info_view.py
+++ datacenter/passcard_info_view.py
@@ -1,11 +1,11 @@
 from datacenter.models import Passcard
 from datacenter.models import Visit
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from datacenter.time_processing import get_duration, format_duration, is_visit_long
 
 
 def passcard_info_view(request, passcode):
-    passcard = Passcard.objects.filter(passcode=passcode)
+    passcard = get_object_or_404(Passcard, passcode=passcode)
     visits_by_passcard = Visit.objects.filter(passcard__passcode=passcode)
     
     this_passcard_visits = []
--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -1,6 +1,6 @@
 from datacenter.models import Passcard
 from datacenter.models import Visit
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from datacenter.time_processing import get_duration, format_duration, is_visit_long
 
 
@@ -12,8 +12,8 @@ def storage_information_view(request):
     employees_in_room = Passcard.objects.filter(visit__leaved_at=None)
 
     for employee_in_room in employees_in_room:
-        employee = Passcard.objects.get(owner_name=employee_in_room.owner_name)
-        active_visit = Visit.objects.get(passcard=employee_in_room, leaved_at=None)
+        employee = get_object_or_404(Passcard, owner_name=employee_in_room.owner_name)
+        active_visit = get_object_or_404(Visit, passcard=employee_in_room, leaved_at=None)
 
         non_closed_visit = {
             'who_entered': employee_in_room.owner_name,
